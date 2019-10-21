137f31fb727534a8600db10d4c46932e427878a2
Jeroen Steenbeeke <jeroen.steenbeeke@topicus.nl>
2019-10-21 10:17:58 +0200

    Added another example of currying

--- src/main/java/nl/topicus/devdev/vavr/Assignment5.java
+++ src/main/java/nl/topicus/devdev/vavr/Assignment5.java
@@ -4,9 +4,12 @@ import io.vavr.CheckedFunction1;
 import io.vavr.Function1;
 import io.vavr.Function2;
 import io.vavr.Function4;
+import io.vavr.Tuple2;
 import io.vavr.control.Option;
+import nl.topicus.devdev.vavr.model.LogLine;
 
 import javax.annotation.Nonnull;
+import java.time.LocalDateTime;
 
 /**
  * Assignment 5 - Fun with functions
@@ -17,27 +20,33 @@ public class Assignment5
 	 * Assignment 5.1 - Partially apply the given function with the given value
 	 */
 	@Nonnull
-	public Function1<Integer,Integer> partialApplication(@Nonnull Integer value, @Nonnull Function2<Integer,Integer,Integer> function) {
+	public Function1<Integer, Integer> partialApplication(@Nonnull Integer value,
+			@Nonnull Function2<Integer, Integer, Integer> function)
+	{
 		return null;
 	}
 
 	/**
 	 * Assignment 5.2a - Make the given unsafe function safe.
-	 *
+	 * 
 	 * Tip: this technique is known as lifting
 	 */
 	@Nonnull
-	public Function2<Integer, Integer, Option<Integer>> makeSafe(@Nonnull Function2<Integer,Integer,Integer> unsafeFunction) {
+	public Function2<Integer, Integer, Option<Integer>> makeSafe(
+			@Nonnull Function2<Integer, Integer, Integer> unsafeFunction)
+	{
 		return null;
 	}
 
 	/**
 	 * Assignment 5.2b - Make the given unsafe function safe.
-	 *
+	 * 
 	 * Tip: this technique is known as lifting
 	 */
 	@Nonnull
-	public Function4<Integer, Integer, Integer, Integer, Option<Integer>> makeSafe(@Nonnull Function4<Integer, Integer, Integer, Integer, Integer> unsafeFunction) {
+	public Function4<Integer, Integer, Integer, Integer, Option<Integer>> makeSafe(
+			@Nonnull Function4<Integer, Integer, Integer, Integer, Integer> unsafeFunction)
+	{
 		return null;
 	}
 
@@ -46,20 +55,21 @@ public class Assignment5
 	 * yields another function (that in turn accepts a single parameter, and so on).
 	 */
 	@Nonnull
-	public <T,U,V,W,X> Function1<T,Function1<U,Function1<V,Function1<W,X>>>> curryFunction(
-			@Nonnull Function4<T, U, V, W, X> inputFunction
-	) {
+	public <T, U, V, W, X> Function1<T, Function1<U, Function1<V, Function1<W, X>>>> curryFunction(
+			@Nonnull Function4<T, U, V, W, X> inputFunction)
+	{
 		return null;
 	}
 
 	/**
 	 * Assignment 5.4 - Compose the two given functions, yielding a single function that
 	 * can convert straight from T to V.
-	 *
+	 * 
 	 * Tip: There are two ways to do this. Pick whichever method you find easiest to read.
 	 */
-	@Nonnull
-	public <T,U,V> Function1<T,V> composition(@Nonnull Function1<T,U> first, @Nonnull Function1<U,V> second) {
+	@Nonnull public <T, U, V> Function1<T, V> composition(@Nonnull Function1<T, U> first,
+			@Nonnull Function1<U, V> second)
+	{
 		return null;
 	}
 
@@ -67,9 +77,24 @@ public class Assignment5
 	 * Assignment 5.5 - Ensure that repeat invocations of the same function use a cached
 	 * version of the function.
 	 */
+	@Nonnull public CheckedFunction1<Integer, Integer> cacheRepeatInvocations(
+			@Nonnull CheckedFunction1<Integer, Integer> function)
+	{
+		return null;
+	}
+
+	/**
+	 * Assignment 5.6 - Currying in action
+	 * 
+	 * Assign the constructor of the class LogLine to a Function4, curry the function, and
+	 * apply the 2 given parameters to this function.
+	 * 
+	 * Return both the curried function, and the partially applied function
+	 */
 	@Nonnull
-	public CheckedFunction1<Integer,Integer> cacheRepeatInvocations(@Nonnull
-			CheckedFunction1<Integer,Integer> function) {
+	public Tuple2<Function1<String, Function1<String, Function1<LocalDateTime, Function1<String, LogLine>>>>, Function1<LocalDateTime, Function1<String, LogLine>>> partiallyAppliedConstructor(
+			@Nonnull String system, @Nonnull String user)
+	{
 		return null;
 	}
 }
--- src/test/java/nl/topicus/devdev/vavr/Assignment5Test.java
+++ src/test/java/nl/topicus/devdev/vavr/Assignment5Test.java
@@ -4,11 +4,15 @@ import io.vavr.CheckedFunction1;
 import io.vavr.Function1;
 import io.vavr.Function2;
 import io.vavr.Function4;
+import io.vavr.Tuple2;
 import io.vavr.control.Option;
+import nl.topicus.devdev.vavr.model.LogLine;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import java.time.LocalDateTime;
 
 import static nl.topicus.devdev.vavr.VavrMatchers.*;
 import static org.hamcrest.CoreMatchers.*;
@@ -17,6 +21,7 @@ import static org.junit.Assume.*;
 
 public class Assignment5Test
 {
+
 	private Assignment5 assignment5;
 
 	@Rule
@@ -32,9 +37,9 @@ public class Assignment5Test
 	}
 
 	@Test
-	public void grade_assignment_5_1() {
-		Function1<Integer, Integer> f1 =
-				assignment5.partialApplication(5, Integer::sum);
+	public void grade_assignment_5_1()
+	{
+		Function1<Integer, Integer> f1 = assignment5.partialApplication(5, Integer::sum);
 
 		assumeThat("An implementation exists", f1, notNullValue());
 		assertThat(f1.apply(5), equalTo(10));
@@ -46,15 +51,17 @@ public class Assignment5Test
 	}
 
 	@Test
-	public void grade_assignment_5_2() {
-		Function2<Integer,Integer,Integer> unsafeDivision = (a,b) -> a / b;
+	public void grade_assignment_5_2()
+	{
+		Function2<Integer, Integer, Integer> unsafeDivision = (a, b) -> a / b;
 
 		Function2<Integer, Integer, Option<Integer>> safeDivision =
 				assignment5.makeSafe(unsafeDivision);
-		Function4<Integer,Integer,Integer,Integer,Integer> unsafeMath = (a,b,c,d) -> a + b / c - d;
+		Function4<Integer, Integer, Integer, Integer, Integer> unsafeMath =
+				(a, b, c, d) -> a + b / c - d;
 
-		Function4<Integer, Integer, Integer, Integer, Option<Integer>>
-				safeMath = assignment5.makeSafe(unsafeMath);
+		Function4<Integer, Integer, Integer, Integer, Option<Integer>> safeMath =
+				assignment5.makeSafe(unsafeMath);
 
 		assumeThat("An implementation exists", safeDivision, notNullValue());
 		assumeThat(safeMath, notNullValue());
@@ -64,7 +71,6 @@ public class Assignment5Test
 		assertThat(safeDivision.apply(4, -2), isSome(-2));
 		assertThat(safeDivision.apply(4, 0), isNone());
 
-
 		assertThat(safeMath.apply(1, 4, 2, 4), isSome(-1));
 		assertThat(safeMath.apply(1, 4, 0, 4), isNone());
 		assertThat(safeMath.apply(1, 1, 1, 1), isSome(0));
@@ -72,8 +78,10 @@ public class Assignment5Test
 	}
 
 	@Test
-	public void grade_assignment_5_3() {
-		Function4<String,Integer,String,Integer,String> uncurried = (a,b,c,d) -> a + b + c + d;
+	public void grade_assignment_5_3()
+	{
+		Function4<String, Integer, String, Integer, String> uncurried =
+				(a, b, c, d) -> a + b + c + d;
 
 		Function1<String, Function1<Integer, Function1<String, Function1<Integer, String>>>>
 				curried = assignment5.curryFunction(uncurried);
@@ -84,11 +92,12 @@ public class Assignment5Test
 	}
 
 	@Test
-	public void grade_assignment_5_4() {
-		Function1<String,Integer> first = String::length;
-		Function1<Integer,Integer> second = a -> a / 2;
+	public void grade_assignment_5_4()
+	{
+		Function1<String, Integer> first = String::length;
+		Function1<Integer, Integer> second = a -> a / 2;
 
-		Function1<String,Integer> composed = assignment5.composition(first, second);
+		Function1<String, Integer> composed = assignment5.composition(first, second);
 
 		assumeThat("An implementation exists", composed, notNullValue());
 		assertThat(composed.apply("Steve"), equalTo(2));
@@ -101,7 +110,7 @@ public class Assignment5Test
 	@Test(timeout = 5000L)
 	public void grade_assignment_5_5() throws Throwable
 	{
-		CheckedFunction1<Integer,Integer> quadraticWithSideEffect = input -> {
+		CheckedFunction1<Integer, Integer> quadraticWithSideEffect = input -> {
 			Thread.sleep(1000L); // Side effect. BAD JEROEN!!
 
 			return input * input;
@@ -113,9 +122,34 @@ public class Assignment5Test
 		assumeThat("An implementation exists", memoized, notNullValue());
 
 		// Should take 50 seconds if not properly memoized
-		for (int i = 0; i < 50; i++) {
+		for (int i = 0; i < 50; i++)
+		{
 			assertThat(memoized.apply(5), equalTo(25));
 		}
+
+	}
+
+	@Test
+	public void grade_assignment_5_6()
+	{
+		final String SYSTEM = "Production";
+		final String USER = "Jeroen";
+		LocalDateTime NOW = LocalDateTime.now();
+		String MESSAGE = "Performing unit test";
+
+		Tuple2<Function1<String, Function1<String, Function1<LocalDateTime, Function1<String, LogLine>>>>, Function1<LocalDateTime, Function1<String, LogLine>>>
+				functions = assignment5.partiallyAppliedConstructor(SYSTEM, USER);
+
+		assumeThat("An implementation exists", functions, notNullValue());
+
+		LogLine expected = new LogLine(SYSTEM, USER, NOW, MESSAGE);
+
+		assertThat(functions._1.apply(SYSTEM)
+				.apply(USER)
+				.apply(NOW)
+				.apply(MESSAGE), equalTo(expected));
+
+		assertThat(functions._2.apply(NOW).apply(MESSAGE), equalTo(expected));
 
 	}
 
